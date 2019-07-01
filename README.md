50f6fcc6f295599bc7c78d4076d65a9a0bc3a7b0
Cesar Cachay <cesarcachay1996@gmail.com>
2019-07-01 01:44:24 -0500

    Add two more methods & new description

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # This repository was created to practice how to build my own npm.
 
-Using this package you will be able to use the methods concat & pop.
+Using this package you will be able to use 4 methods where:
+
+- You will be able to use concatJS & popJS I changed their names to make sure I exported this package properly
+- You will be able to use 2 methods used in ruby those are "empty & capitalize" but I added JS in their names.
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-module.exports.concatJS = function() {
-  const concat = (arr, ...elements) => {
-    for (let i = 0; i < elements.length; i++) {
-      arr.push(elements[i]);
-    }
-    return arr;
-  };
+const concatJS = (arr, ...elements) => {
+  for (let i = 0; i < elements.length; i++) {
+    arr.push(elements[i]);
+  }
+  return arr;
 };
 
-module.exports.popJS = function() {
-  const pop = function(arr) {
-    const lastItem = arr[arr.length - 1];
-    arr.length = arr.length - 1;
-    return lastItem;
-  };
+const popJS = function(arr) {
+  const lastItem = arr[arr.length - 1];
+  arr.length = arr.length - 1;
+  return lastItem;
 };
+
+const emptyJS = something => {
+  return something.length === 0;
+};
+
+const capitalizeJS = string => {
+  return `${string[0].toUpperCase()}${string.slice(1)}`;
+};
+
+export { concatJS, popJS, emptyJS, capitalizeJS };
